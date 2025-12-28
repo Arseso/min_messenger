666a59725e36128564134ef016964a47ca0c9912
Artem Sokolov <arseso.main@gmail.com>
2025-12-28 19:27:57 +0300

    models module initialization

---
+++ src/ContentFilter/module_api/models/__init__.py
@@ -0,0 +1,5 @@
+from module_api.models.TextRequest import TextRequest
+from module_api.models.TextResponse import TextResponse
+from module_api.models.TextStatusRequest import TextStatusRequest, STATUS
+from module_api.models.TextStatusResponse import TextStatusResponse
+
