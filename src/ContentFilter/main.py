5fb5037d6e63ce5fe1ada92a3806fff394279920
Artem Sokolov <arseso.main@gmail.com>
2025-12-28 23:43:46 +0300

    main imports changes

--- src/ContentFilter/main.py
+++ src/ContentFilter/main.py
@@ -1,5 +1,7 @@
-import module_api.api as api
+from module_api import run_api
+import module_nn.nn_main as nn
 from env import Settings
 
 if __name__ == "__main__":
-    api.start(Settings.SERVER_HOST, Settings.SERVER_PORT)
+    run_api(Settings.SERVER_HOST, Settings.SERVER_PORT)
+    
