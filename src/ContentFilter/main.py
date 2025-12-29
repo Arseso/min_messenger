bea92d73326707d69269f0619bfa320bc4c61cca
Artem Sokolov <arseso.main@gmail.com>
2025-12-29 21:56:35 +0300

    main file definition

--- src/ContentFilter/main.py
+++ src/ContentFilter/main.py
@@ -1,7 +1,14 @@
+import threading
+
 from module_api import run_api
+from module_nn import run_nn
 import module_nn.nn_main as nn
 from env import Settings
 
 if __name__ == "__main__":
-    run_api(Settings.SERVER_HOST, Settings.SERVER_PORT)
+    neural_net = threading.Thread(target=run_nn)
+    neural_net.start()
+    
+    run_api()
+    
     
