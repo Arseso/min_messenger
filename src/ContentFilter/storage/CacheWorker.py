33727fd4478607b8ca9eaa94e866c77b224d4563
Artem Sokolov <arseso.main@gmail.com>
2025-12-29 16:31:27 +0300

    Redis workers

---
+++ src/ContentFilter/storage/CacheWorker.py
@@ -0,0 +1,46 @@
+from module_api import TextRequest
+from pydantic_core import from_json
+
+from redis import Redis
+
+
+class CacheWorker:
+    def __init__(self, redis_host: str, redis_port: int, redis_pwd: str) -> None:
+        self.redis = Redis(
+            host=redis_host,
+            port=redis_port,
+            password=redis_pwd,
+            db= 0
+        )
+        
+    def append(self, obj: TextRequest):
+        status = self.redis.append(obj.id, obj.model_dump_json())
+        if not status:
+            raise Exception("Error while adding an object to queue")
+    
+    def has_key(self, id: str) -> bool:
+        if self.redis.get(id):
+            return True
+        return False
+    
+    def get(self, key: str) -> TextRequest | None:
+        value = self.redis.get(key)
+        if not value:
+            return None
+        self.redis.delete(key)
+        return TextRequest.model_validate(from_json(value))
+    
+
+if __name__ == "__main__":
+    from env import Settings
+
+    redis = CacheWorker("localhost", Settings.REDIS_PORT, Settings.REDIS_PWD)
+    value = TextRequest(id= 'aaaddd', text = "lol")
+
+    assert redis.append(value) == None
+    assert redis.has_key(value.id) == True
+    assert redis.get(value.id) == value
+    assert redis.has_key(value.id) == False
+    assert redis.get(value.id) == None
+    print("---- ALL TESTS SUCCESSFULLY ----")
+    
---
+++ src/ContentFilter/storage/QueueWorker.py
@@ -0,0 +1,53 @@
+from typing import Optional
+
+from redis import Redis
+
+from pydantic_core import from_json
+from module_api import TextRequest
+
+class QueueWorker:
+    def __init__(self, 
+                 host: str = "localhost",
+                 port: int = 6379,
+                 password: str = "",
+                 db: int = 0,
+                 queue_name: str = "string_queue"):
+        self.redis = Redis(
+            host=host,
+            port=port,
+            password=password,
+            db=db,
+            decode_responses=True
+        )
+        self.queue_name = queue_name
+    
+    def append(self, item: TextRequest) -> bool:
+        item_json = item.model_dump_json()
+        return self.redis.rpush(self.queue_name, item_json) > 0
+    
+    def pop(self) -> Optional[str]:
+        json = self.redis.lpop(self.queue_name)
+        return TextRequest(**from_json(json))
+    
+    def size(self) -> int:
+        return self.redis.llen(self.queue_name)
+    
+    def clear(self) -> None:
+        self.redis.delete(self.queue_name)
+
+if __name__ == "__main__":
+    from env import Settings
+
+    queue = QueueWorker("localhost", Settings.REDIS_PORT, Settings.REDIS_PWD)
+    value1 = TextRequest(id= 'aaaddd', text = "lol")
+    value2 = TextRequest(id= 'dddaaa', text = "olo")
+
+    assert queue.clear() == None
+    assert queue.size() == 0
+    assert queue.append(value1) == True
+    assert queue.append(value2) == True
+    assert queue.size() == 2
+    assert queue.pop() == value1
+    assert queue.pop() == value2
+    assert queue.size() == 0
+    print("---- ALL TESTS SUCCESSFULLY ----")
