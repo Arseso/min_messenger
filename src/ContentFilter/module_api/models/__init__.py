afbd9a29c4d23f184bfd44daff06b26976a0cd4c
Artem Sokolov <arseso.main@gmail.com>
2025-12-29 17:12:21 +0300

    Error model

--- src/ContentFilter/module_api/models/__init__.py
+++ src/ContentFilter/module_api/models/__init__.py
@@ -2,4 +2,5 @@ from module_api.models.TextRequest import TextRequest
 from module_api.models.TextResponse import TextResponse
 from module_api.models.TextStatusRequest import TextStatusRequest, STATUS
 from module_api.models.TextStatusResponse import TextStatusResponse
+from module_api.models.Error import Error
 
